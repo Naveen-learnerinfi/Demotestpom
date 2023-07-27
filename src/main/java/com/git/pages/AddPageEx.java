f5156283b32573cf35a11fe52c10e8be31553243
unknown <rnaveenkumar826@gmail.com>
2023-07-27 21:05:21 +0530

    added DemoPage and Modified AddPage

--- src/main/java/com/git/pages/AddPageEx.java
+++ src/main/java/com/git/pages/AddPageEx.java
@@ -1,5 +1,21 @@
 package com.git.pages;
 
+
 public class AddPageEx {
+	void typeex() {
+		System.out.println("i am typing something");
+	}
+	void swimming() {
+		System.out.println("i am swimming");
+	}
+	
+	
+	public static void main(String args[]) {
+	AddPageEx ap = new AddPageEx();
+	ap.typeex();
+	ap.swimming();
+	
+	}
+	
 
 }
---
+++ src/main/java/com/git/pages/DemoPageEx.java
@@ -0,0 +1,10 @@
+package com.git.pages;
+
+public class DemoPageEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
